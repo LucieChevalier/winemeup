9df8f8d38f362148859408324f2380723b8f74a2
Maxime Gueye <gueye.maxime1994@gmail.com>
2022-08-30 16:02:56 +0200

    Creation de la methode show dans le controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,15 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:event_id])
+    @current_user = current_user
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :address, :date, :category, :level, :host_request, :max_number_guest, :min_price, :max_price)
+  end
 end
