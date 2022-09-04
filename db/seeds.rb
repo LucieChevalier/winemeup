1541c521a04c8944f96d9ac5ea9bc9f3101e3872
Julien Lamy <lamy.j8@gmail.com>
2022-09-04 10:53:48 +0200

    seeds users

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ user1 = User.create!(
   first_name: 'Julien',
   last_name: 'Bardy',
   email: 'julien.bardy@gmail.com',
-  password: 'JulienB',
+  password: '123456',
   address: '49 quai Émile Cormerais',
   level: 'Novice',
   postal_code: '44800',
@@ -36,7 +36,7 @@ user2 = User.create!(
   first_name: 'Maria',
   last_name: 'Cabrero',
   email: 'maria.cabrero@gmail.com',
-  password: 'MariaC',
+  password: '123456',
   address: '8 passage de la Poule Noire',
   level: 'Intermédiaire',
   postal_code: '44000',
@@ -51,7 +51,7 @@ user3 = User.create!(
   first_name: 'Jean',
   last_name: 'Dupont',
   email: 'jean.dupont@gmail.com',
-  password: 'JeanDu',
+  password: '123456',
   address: '12 rue Paul Bellamy',
   level: 'Expert',
   postal_code: '44000',
@@ -62,15 +62,167 @@ file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu
 user3.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
 user3.save
 
+user4 = User.create!(
+  first_name: 'Alice',
+  last_name: 'Hay',
+  email: 'alice.hay@gmail.com',
+  password: '123456',
+  address: '33 rue Saint-Léonard',
+  level: 'Novice',
+  postal_code: '44000',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user4.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user4.save
+
+user5 = User.create!(
+  first_name: 'Thierry',
+  last_name: 'Dabo',
+  email: 'thierry.dabo@gmail.com',
+  password: '123456',
+  address: '2 place Aristide Briand',
+  level: 'Intermédiaire',
+  postal_code: '44000',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user5.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user5.save
+
+user6 = User.create!(
+  first_name: 'Alex',
+  last_name: 'Plantey',
+  email: 'alex.plantey@gmail.com',
+  password: '123456',
+  address: '11 avenue de Normandie',
+  level: 'Expert',
+  postal_code: '44300',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user6.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user6.save
+
+user7 = User.create!(
+  first_name: 'Vincent',
+  last_name: 'Briat',
+  email: 'vincent.briat@bouygues.fr',
+  password: '123456',
+  address: '12 rue Paul Bellamy',
+  level: 'Novice',
+  postal_code: '44200',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user7.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user7.save
+
+user8 = User.create!(
+  first_name: 'Nicolas',
+  last_name: 'Ramtoula',
+  email: 'nicolas.ramtoula@orange.fr',
+  password: '123456',
+  address: "4 rue de la tour d'auvergne",
+  level: 'Intermédiaire',
+  postal_code: '44200',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user8.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user8.save
+
+user9 = User.create!(
+  first_name: 'Bertand',
+  last_name: 'Lefort',
+  email: 'bertand.lefort@free.fr',
+  password: '123456',
+  address: '198 rue du Général Buat',
+  level: 'Expert',
+  postal_code: '44109',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user9.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user9.save
+
+user10 = User.create!(
+  first_name: 'Laure',
+  last_name: 'Carette',
+  email: 'laure.carette@gmail.com',
+  password: '123456',
+  address: '21 allé des Tanneurs',
+  level: 'Novice',
+  postal_code: '44000',
+  city: 'Nantes'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user10.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user10.save
+
+
+user11 = User.create!(
+  first_name: 'Albert',
+  last_name: 'Gilbert',
+  email: 'albert.gilbert@gmail.com',
+  password: '123456',
+  address: '12 villa de la Renaissance',
+  level: 'Intermédiaire',
+  postal_code: '75019',
+  city: 'Paris'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user11.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user11.save
+
+
+user12 = User.create!(
+  first_name: 'Patrick',
+  last_name: 'Gaussent',
+  email: 'patrick.gaussent@gmail.com',
+  password: '123456',
+  address: "2 rue de l'Arbre sec",
+  level: 'Expert',
+  postal_code: '69000',
+  city: 'Lyon'
+
+)
+file = URI.open("https://www.feter-recevoir.com/upload/image/deguisement-pikachu-bebe-p-image-178360-grande.jpg")
+user12.photo.attach(io: file, filename: "costume-1.png", content_type: "image/png")
+user12.save
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 puts "Creating events..."
 
 loire = Event.create!(
   name: 'descente de la loire en pinard',
-  description: "Je vous invite dans mon jardin, sous le pont de cheviré pour une descente de la loire par ses pinards : du côte roannaise en Auvergne jusqu'à notre Muscadet local",
+  description: "Soyez les bienvenus dans mon jardin, sous le pont de cheviré pour une descente de la loire par ses pinards : du côte roannaise en Auvergne jusqu'à notre Muscadet local",
   address: "49 quai Émile Cormerais, 44800 Saint-Herblain",
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: "loire",
+  category: "Loire",
   level: 'Novice',
   host_request: '1 côte-roannaise et 1 muscadet-sèvre-et-maine',
   max_number_guest: 12,
@@ -79,9 +231,7 @@ loire = Event.create!(
   host: user1
 )
 
-file = URI.open("https://cap.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcap.2F2019.2F08.2F04.2Fa843fc6f-5f7c-43e3-aec0-89f62c93a745.2Ejpeg/1200x630/background-color/ffffff/quality/70/dormez-comme-un-roi-et-sauvez-le-chateau-de-chambord-1346662.jpg")
-loire.photo.attach(io: file, filename: "loire.png", content_type: "image/png")
-loire.save
+
 
 
 champagne = Event.create!(
@@ -90,7 +240,7 @@ champagne = Event.create!(
   address:'49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Nov 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Champagne",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
@@ -99,9 +249,7 @@ champagne = Event.create!(
   host: user1
 )
 
-file = URI.open("https://cdn.aveine.paris/blog/wp-content/uploads/2021/06/09141104/Tour-de-France-regions-viticoles-Cave-Champagne.jpg")
-champagne.photo.attach(io: file, filename: "champagne.png", content_type: "image/png")
-champagne.save
+
 
 alsace_lorraine = Event.create!(
   name: "retour de route des vins alsaciens",
@@ -109,17 +257,15 @@ alsace_lorraine = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Nov 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Alsace-Lorraine",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user3
+  host: user1
 )
-file = URI.open("https://cdn.aveine.paris/blog/wp-content/uploads/2021/06/09142030/Tour-de-France-regions-viticoles-Route-des-vins-Alsace.jpg")
-alsace_lorraine.photo.attach(io: file, filename: "alsace-lorraine.png", content_type: "image/png")
-alsace_lorraine.save
+
 
 bourgogne = Event.create!(
   name: 'nuit blanche au côte de nuits',
@@ -127,17 +273,15 @@ bourgogne = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Nov 2022'),
   time: Time.parse('17:30'),
-  category: :loire,
+  category: "Bourgogne",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user2
 )
-file = URI.open("https://cdn.aveine.paris/blog/wp-content/uploads/2021/06/09142442/Tour-de-France-regions-viticoles-Hospices-de-Beaune.jpg")
-bourgogne.photo.attach(io: file, filename: "bourgogne.png", content_type: "image/png")
-bourgogne.save
+
 
 jura_savoie_bugey = Event.create!(
   name: 'dégustation des vins de nos montagnes',
@@ -145,17 +289,15 @@ jura_savoie_bugey = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Nov 2022'),
   time: Time.parse('17:30'),
-  category: :loire,
+  category: "Jura-Bugey-Savoie",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user2
 )
-file = URI.open("https://img.lemde.fr/2022/04/20/0/0/5433/3578/664/0/75/0/19246b4_1650467126993-pns-900571516.jpg")
-jura_savoie_bugey.photo.attach(io: file, filename: "jura_savoie_bugey.png", content_type: "image/png")
-jura_savoie_bugey.save
+
 
 beaujolais = Event.create!(
   name: 'soirée beaujolais pas nouveau',
@@ -163,17 +305,15 @@ beaujolais = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Nov 2022'),
   time: Time.parse('17:30'),
-  category: :loire,
+  category: "Beaujolais",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user2
 )
-file = URI.open("https://www.lavoixdelain.fr/photos/moyen/39703.jpg")
-beaujolais.photo.attach(io: file, filename: "beaujolais.png", content_type: "image/png")
-beaujolais.save
+
 
 vallee_du_rhone = Event.create!(
   name: 'vallée du rhône méridonale et charcuterie',
@@ -181,17 +321,15 @@ vallee_du_rhone = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('17:30'),
-  category: :loire,
+  category: "Rhône",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user3
 )
-file = URI.open("https://cdn.aveine.paris/blog/wp-content/uploads/2021/06/09142144/Tour-de-France-regions-viticoles-Palais-des-Papes.jpg")
-vallee_du_rhone.photo.attach(io: file, filename: "valee_du_rhone.png", content_type: "image/png")
-vallee_du_rhone.save
+
 
 corse = Event.create!(
   name: 'vins corsés et fromages qui puent',
@@ -199,17 +337,15 @@ corse = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Corse",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user3
 )
-file = URI.open("https://www.allibert-trekking.com/uploads/media/images/thumbnails/AMin3_2027-balisage-gr20.jpeg?v5")
-corse.photo.attach(io: file, filename: "corse.png", content_type: "image/png")
-corse.save
+
 
 provence = Event.create!(
   name: 'pic-nique arrosé aux vins de provence',
@@ -217,17 +353,15 @@ provence = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Provence",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user3
 )
-file = URI.open("https://media.istockphoto.com/photos/lavender-in-provence-at-sunrise-picture-id1133997327?k=20&m=1133997327&s=612x612&w=0&h=B5sIQ-f2gCvvZPpk1FW_uIwGnn_nF79bK9yIDl6M0aw=")
-provence.photo.attach(io: file, filename: "provence.png", content_type: "image/png")
-provence.save
+
 
 languedoc_roussillon = Event.create!(
   name: 'dégustation de nos vins du Roussillon',
@@ -235,17 +369,15 @@ languedoc_roussillon = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Languedoc-Roussillon",
   level: 'Novice',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user4
 )
-file = URI.open("https://cdn.generationvoyage.fr/2020/11/couverture-pont-du-gard.jpg")
-languedoc_roussillon.photo.attach(io: file, filename: "languedoc_roussillon.png", content_type: "image/png")
-languedoc_roussillon.save
+
 
 sud_ouest = Event.create!(
   name: 'garden party et vins de notre pays basque',
@@ -253,17 +385,15 @@ sud_ouest = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
-  level: 'Novice',
+  category: "Sud-Ouest",
+  level: 'Intermédiaire',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user4
 )
-file = URI.open("https://media.istockphoto.com/photos/red-espelette-peppers-drying-in-the-wall-of-basque-house-picture-id502807780?k=20&m=502807780&s=612x612&w=0&h=XJtkTZelu_9ntRznhitoupIJ61KaYCfj604wcKen8Sc=")
-sud_ouest.photo.attach(io: file, filename: "sud_ouest.png", content_type: "image/png")
-sud_ouest.save
+
 
 bordeaux = Event.create!(
   name: "dégustation premium d'entre-deux-mers",
@@ -271,17 +401,43 @@ bordeaux = Event.create!(
   address: '49 quai Émile Cormerais, 44800 Saint-Herblain',
   date: Date.parse('3rd Feb 2022'),
   time: Time.parse('08:30'),
-  category: :loire,
+  category: "Bordeaux",
   level: 'Expert',
   host_request: '',
   max_number_guest: '',
   min_price: 8,
   max_price: 12,
-  host: user1
+  host: user4
 )
-file = URI.open("https://static-otelico.com/cache/seekoo/0-La-Cite-du-Vin-reflets-1---Axelferis-7.jpg")
-bordeaux.photo.attach(io: file, filename: "bordeaux.png", content_type: "image/png")
-bordeaux.save
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 puts "Creating bottles..."
 
@@ -466,47 +622,58 @@ muscadet_sèvre_et_maine = Bottle.create!(
   user: user2
 )
 
+
+
+
+
+
+
+
+
+
+
+
 puts "Creating bookings..."
 
 booking_1 = Booking.create!(
   status: 'Upcoming',
   comment: '',
-  guest: user3,
+  guest: user5,
   event: loire
 )
 
 booking_2 = Booking.create!(
   status: 'Upcoming',
   comment: '',
-  guest: user3,
+  guest: user6,
   event: bourgogne
 )
 
 booking_3 = Booking.create!(
   status: 'Upcoming',
   comment: '',
-  guest: user2,
+  guest: user7,
   event: loire
 )
 
 booking_4 = Booking.create!(
   status: 'Upcoming',
   comment: '',
-  guest: user3,
+  guest: user8,
   event: champagne
 )
 
   booking_5 = Booking.create!(
   status: 'Upcoming',
   comment: '',
-  guest: user2,
+  guest: user9,
   event: champagne
 )
 
 booking_6 = Booking.create!(
   status: 'Past',
   comment: '',
-  guest: user2,
+  guest: user10,
   event: bourgogne
 )
 
