adb73b37fc5057acdd4020668fa47d4fadbefdf7
jordan barreix <jordan@barreix.com>
2022-09-07 19:04:36 +0200

    langing page responsive

--- app/javascript/controllers/prevent_default_controller.js
+++ app/javascript/controllers/prevent_default_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="prevent-default"
 export default class extends Controller {
-  connect() {
-    console.log("Coucou")
-  }
 
   preventDefaultClick(event) {
     event.preventDefault()
